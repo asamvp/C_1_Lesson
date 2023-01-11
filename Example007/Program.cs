2ecaa20655bfd99ed9857f0cc77a465fbf487f0a
Asam <asamvp@mail.ru>
2023-01-11 22:36:04 +0500

    7 урок

---
+++ Example007/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
+int xa = 1; 
+int ya = 1;
+int xb = 1;
+int yb = 5;
+int xc = 5; 
+int yc = 5;
+
+Console.SetCursorPosition(xa, ya);
+Console.Write("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.Write("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.Write("+");
